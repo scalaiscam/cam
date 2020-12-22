7eb3a2bfcc593434597e5d4592e9fe1295514510
Camille Scalais <camille_scalais@hotmail.com>
2020-12-22 16:03:15 +0100

    js ok

--- js/scripts.js
+++ js/scripts.js
@@ -25,42 +25,42 @@ function loadmeteo(ville){
                     $('.daycurrent h2').html(monObjet2.current_condition.condition);
                     //$('.daycurrent img').attr('src',monObjet2.current_condition.icon_big);
                     $('.daycurrent .image').attr('class',"image image-"+monObjet2.current_condition.condition_key);
-                    $('.daycurrent .jour').html(monObjet2.fcst_day_0.day_long);
+                    $('.daycurrent .heure').html(monObjet2.current_condition.hour);
                     $('.daycurrent .date').html(monObjet2.current_condition.date);
-                    $('.daycurrent .temperature').html(monObjet2.current_condition.tmp+'Â°c');
+                    $('.daycurrent .temperature').html(monObjet2.current_condition.tmp+' °c');
+                    $('.daycurrent .humidite').html(monObjet2.current_condition.humidity+' %');
 
 
                     $('.day1 h2').html(monObjet2.fcst_day_1.condition);
                     //$('.day1 img').attr('src',monObjet2.fcst_day_1.icon_big);
                     $('.day1 .image').attr('class',"image image-"+monObjet2.fcst_day_1.condition_key);
-                    $('.day1 .jour').html(monObjet2.fcst_day_1.day_long);
-                    $('.day1 .date').html(monObjet2.fcst_day_1.date);
-                    $('.day1 .temperaturemax').html('tempÃ©rature max : '+monObjet2.fcst_day_1.tmax+'Â°c');
-                    $('.day1 .temperaturemin').html('tempÃ©rature min : '+monObjet2.fcst_day_1.tmin+'Â°c');
+                    $('.day1 .date').html(monObjet2.fcst_day_1.day_long);
+                    $('.day1 .temperaturemax').html(monObjet2.fcst_day_1.tmax);
+                    $('.day1 .temperaturemin').html(' / '+monObjet2.fcst_day_1.tmin+'°c');
+
 
                     $('.day2 h2').html(monObjet2.fcst_day_2.condition);
                     //$('.day2 img').attr('src',monObjet2.fcst_day_2.icon_big);
                     $('.day2 .image').attr('class',"image image-"+monObjet2.fcst_day_2.condition_key);
-                    $('.day2 .jour').html(monObjet2.fcst_day_2.day_long);
-                    $('.day2 .date').html(monObjet2.fcst_day_2.date);
-                    $('.day2 .temperaturemax').html('tempÃ©rature max : '+monObjet2.fcst_day_2.tmax+'Â°c');
-                    $('.day2 .temperaturemin').html('tempÃ©rature min : '+monObjet2.fcst_day_2.tmin+'Â°c');
+                    $('.day2 .date').html(monObjet2.fcst_day_2.day_long);
+                    $('.day2 .temperaturemax').html(monObjet2.fcst_day_2.tmax);
+                    $('.day2 .temperaturemin').html(' / '+monObjet2.fcst_day_2.tmin+'°c');
+
 
                     $('.day3 h2').html(monObjet2.fcst_day_3.condition);
                     //$('.day3 img').attr('src',monObjet2.fcst_day_3.icon_big);
                     $('.day3 .image').attr('class',"image image-"+monObjet2.fcst_day_3.condition_key);
-                    $('.day3 .jour').html(monObjet2.fcst_day_3.day_long);
-                    $('.day3 .date').html(monObjet2.fcst_day_3.date);
-                    $('.day3 .temperaturemax').html('tempÃ©rature max : '+monObjet2.fcst_day_3.tmax+'Â°c');
-                    $('.day3 .temperaturemin').html('tempÃ©rature min : '+monObjet2.fcst_day_3.tmin+'Â°c');
+                    $('.day3 .date').html(monObjet2.fcst_day_3.day_long);
+                    $('.day3 .temperaturemax').html(monObjet2.fcst_day_3.tmax);
+                    $('.day3 .temperaturemin').html(' / '+monObjet2.fcst_day_3.tmin+'°c');
+
 
                     $('.day4 h2').html(monObjet2.fcst_day_4.condition);
                     //$('.day4 img').attr('src',monObjet2.fcst_day_4.icon_big);
                     $('.day4 .image').attr('class',"image image-"+monObjet2.fcst_day_4.condition_key);
-                    $('.day4 .jour').html(monObjet2.fcst_day_4.day_long);
-                    $('.day4 .date').html(monObjet2.fcst_day_4.date);
-                    $('.day4 .temperaturemax').html('tempÃ©rature max : '+monObjet2.fcst_day_4.tmax+'Â°c');
-                    $('.day4 .temperaturemin').html('tempÃ©rature min : '+monObjet2.fcst_day_4.tmin+'Â°c');
+                    $('.day4 .date').html(monObjet2.fcst_day_4.day_long);
+                    $('.day4 .temperaturemax').html(monObjet2.fcst_day_4.tmax);
+                    $('.day4 .temperaturemin').html(' / '+monObjet2.fcst_day_4.tmin+'°c');
 
                     }
 
@@ -118,3 +118,12 @@ function loadmeteo(ville){
         $("input[type=text]").val("");
         loadmeteo(ville);
     });
+
+
+
+const switchButton = document.getElementById('switch');
+ 
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+});
