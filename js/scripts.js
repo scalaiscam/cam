c1724199bc1270f54aca2a45adbcf90c88bfc685
Camille Scalais <camille_scalais@hotmail.com>
2021-01-07 22:24:19 +0100

    finish

---
+++ js/scripts.js
@@ -0,0 +1,110 @@
+
+function loadmeteo(ville){
+    var url2 = "https://www.prevision-meteo.ch/services/json/"+ville;
+
+                $.ajax({
+
+                    url : url2,
+        
+                    type : 'GET',
+        
+                    dataType : 'json',
+        
+                    success : function(monObjet2){
+        
+                    console.log(monObjet2)
+
+                    $('h1').html(monObjet2.city_info.name+", "+monObjet2.city_info.country);
+                    $('.daycurrent h2').html(monObjet2.current_condition.condition);
+                    //$('.daycurrent img').attr('src',monObjet2.current_condition.icon_big);
+                    $('.daycurrent .image').attr('class',"image image-"+monObjet2.current_condition.condition_key);
+                    $('.daycurrent .heure').html(monObjet2.current_condition.hour);
+                    $('.daycurrent .date').html(monObjet2.current_condition.date);
+                    $('.daycurrent .temperature').html(monObjet2.current_condition.tmp+' °c');
+                    $('.daycurrent .humidite').html(monObjet2.current_condition.humidity+' %');
+
+
+                  
+                    //$('.day1 img').attr('src',monObjet2.fcst_day_1.icon_big);
+                    $('.day1 .image').attr('class',"image image-"+monObjet2.fcst_day_1.condition_key);
+                    $('.day1 .date h3').html(monObjet2.fcst_day_1.day_long);
+                    $('.day1 .temperaturemax').html(monObjet2.fcst_day_1.tmax);
+                    $('.day1 .temperaturemin').html(' / '+monObjet2.fcst_day_1.tmin+'°c');
+
+
+                  
+                    //$('.day2 img').attr('src',monObjet2.fcst_day_2.icon_big);
+                    $('.day2 .image').attr('class',"image image-"+monObjet2.fcst_day_2.condition_key);
+                    $('.day2 .date h3').html(monObjet2.fcst_day_2.day_long);
+                    $('.day2 .temperaturemax').html(monObjet2.fcst_day_2.tmax);
+                    $('.day2 .temperaturemin').html(' / '+monObjet2.fcst_day_2.tmin+'°c');
+
+
+                    //$('.day3 img').attr('src',monObjet2.fcst_day_3.icon_big);
+                    $('.day3 .image').attr('class',"image image-"+monObjet2.fcst_day_3.condition_key);
+                    $('.day3 .date h3').html(monObjet2.fcst_day_3.day_long);
+                    $('.day3 .temperaturemax').html(monObjet2.fcst_day_3.tmax);
+                    $('.day3 .temperaturemin').html(' / '+monObjet2.fcst_day_3.tmin+'°c');
+
+
+                   
+                    //$('.day4 img').attr('src',monObjet2.fcst_day_4.icon_big);
+                    $('.day4 .image').attr('class',"image image-"+monObjet2.fcst_day_4.condition_key);
+                    $('.day4 .date h3').html(monObjet2.fcst_day_4.day_long);
+                    $('.day4 .temperaturemax').html(monObjet2.fcst_day_4.tmax);
+                    $('.day4 .temperaturemin').html(' / '+monObjet2.fcst_day_4.tmin+'°c');
+
+                    }
+
+                })//ajax success
+}//loadmeteo
+
+    
+    navigator.geolocation.getCurrentPosition(function(position){
+        console.log(position);
+
+        var lng = position.coords.longitude;
+        var lat = position.coords.latitude;
+
+        console.log("lat: "+lat+" lng: "+lng)
+
+        var url = "https://us1.locationiq.com/v1/reverse.php?key=pk.c4245aea755e71277f98399333e29db8&format=json&lat="+lat+"&lon="+lng;
+   
+        $.ajax({
+
+            url : url,
+
+            type : 'GET',
+
+            dataType : 'json',
+
+            success : function(monObjet){
+
+                ville = monObjet.address.town;
+                console.log(ville); 
+                              
+                loadmeteo(ville);      
+
+            }//success
+        })//ajax
+    })//geoloc
+
+//recherche
+    btnrecherche = document.getElementById('formweather').addEventListener("submit", function(event) {
+    
+        event.preventDefault();
+    }, false);
+
+
+    $("#go").click(function(){
+        var ville = $("#ville").val();
+        console.log(ville);
+        loadmeteo(ville);
+    }); //go
+
+    $("#localisation").click(function(e){
+        e.preventDefault();
+        $("input[type=text]").val("");
+        loadmeteo(ville);
+    });//localisation
+
